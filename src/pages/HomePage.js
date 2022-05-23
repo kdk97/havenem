940796d6029f95b348a83b24bbda8a8f00caed86
kdk97 <kevinkdk97@gmail.com>
2022-05-23 12:30:47 +0200

    hotfix

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,18 @@
-import logo from "../assets/img/logo.png"
+import logo from "../assets/img/skovl-logo.png"
+import { Link } from "react-router-dom";
+
 export default function HomePage() {
 
 
     return(
-          <h1>Havenem</h1>
+        <div className="grid-container">
+          <img className="logo" src={logo} alt="skovl-logo" />
+          <button>
+            <Link to="/opret-bruger">Opret bruger</Link>
+          </button>
+          <button>
+            <Link to="/log-ind">Log ind</Link>
+          </button>
+          </div>
     )
 }
