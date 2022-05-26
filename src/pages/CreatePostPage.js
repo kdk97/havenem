b513aa68fb2b85d0d5853c762bc06af115a63147
mikkelhau <mikkel.hau@hotmail.com>
2022-05-26 21:54:04 +0200

    CreatePost v0.5

--- src/pages/CreatePostPage.js
+++ src/pages/CreatePostPage.js
@@ -1,4 +1,13 @@
+import SelectPostImage from "../components/SelectPostImage"
+import React from 'react'
 
 export default function CreatePostPage() {
+
+    return (
+        <div>
+            <h1>Opret annonce</h1>
+            <SelectPostImage />
+        </div>
+    )
 
 }
