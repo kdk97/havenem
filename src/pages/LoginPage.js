43681f7a382dc8554d93883053d613a8e027da28
kdk97 <kevinkdk97@gmail.com>
2022-05-26 22:03:35 +0200

    loginpage

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,18 +1,53 @@
 import { useEffect, useState } from "react";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { Link } from "react-router-dom";
+import React from "react";
+import logo from "../assets/img/skovl-logo.png";
 
+export default function SignInPage({ showLoader }) {
+  const [errorMessage, setErrorMessage] = useState("");
+  const auth = getAuth();
 
+  useEffect(() => {
+    showLoader(false);
+  }, [showLoader]);
 
-export default function LoginPage({ showLoader }) {
-    const [errorMessage, setErrorMessage] = useState("");
-    const auth = getAuth();
-  
-    useEffect(() => {
-      showLoader(false);
-    }, [showLoader]);
+  function signIn(event) {
+    event.preventDefault();
+    const mail = event.target.mail.value; 
+    const password = event.target.password.value; 
 
-return(
-    <h1>LoginPage</h1>
-)
-
+    signInWithEmailAndPassword(auth, mail, password)
+      .then((userCredential) => {
+        
+        const user = userCredential.user;
+        console.log(user); 
+      })
+      .catch((error) => {
+        let code = error.code; 
+        console.log(code);
+        code = code.replaceAll("-", " "); 
+        code = code.replaceAll("auth/", "");
+        setErrorMessage(code);
+      });
+  }
+  return (
+    <section className="page">
+      <img className="logo" src={logo} alt="Logo" />
+      <h1>Fitness Tracker</h1>
+      <form onSubmit={signIn}>
+        <input type="email" name="mail" placeholder="Indtast din email" />
+        <input
+          type="password"
+          name="password"
+          placeholder="Indtast dit kodeord"
+        />
+        <p className="text-error">{errorMessage}</p>
+        <button>Log ind</button>
+      </form>
+      <p className="text-center1">
+        Har du ikke en bruger? <Link to="/sign-up">Opret</Link>
+      </p>
+    </section>
+  );
 }
