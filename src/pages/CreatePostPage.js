7c20195452c6effeb1fd91c959bf04006ef4d77d
mikkelhau <mikkel.hau@hotmail.com>
2022-05-31 13:53:06 +0200

    Progress

--- src/pages/CreatePostPage.js
+++ src/pages/CreatePostPage.js
@@ -1,15 +1,30 @@
-import SelectPostImage from "../components/SelectPostImage"
-import React from 'react'
-import SearchBar from "../components/SearchBar";
-
+import React from 'react';
+import { addDoc, serverTimestamp } from "@firebase/firestore";
+import { postsRef } from "../firebase-config";
+import CreatePost from "../components/CreatePost";
+import { useNavigate } from "react-router-dom";
+import { getAuth } from "firebase/auth";
 
 export default function CreatePostPage() {
 
-    return (
-        <div>
-            <h1>Opret annonce</h1>
-            <SelectPostImage />
-        </div>
-    )
+    const auth = getAuth();
+    const navigate = useNavigate();
+    
+    async function handleSubmitPost (newPost) {
+      console.log(handleSubmitPost)
+      // showLoader(true); show the loader
+      newPost.createdAt = serverTimestamp(); // timestamp (now)
+      newPost.uid = auth.currentUser.uid; // uid of auth user / signed in user
+      await addDoc(postsRef, newPost); // add new doc - new post object
 
+      // showLoader(false); hide the loader
+      navigate("/udlej-færdig");
+  };
+
+    return (
+      <div>
+      <h1>Opret annonce</h1>
+      <CreatePost savePost={handleSubmitPost} />
+      </div>
+    )
 }
--- src/pages/PostCreatedPage.js
+++ src/pages/PostCreatedPage.js
@@ -1,11 +1,21 @@
 import succes from "../assets/img/succes.png"
 
-export default function () {
+export default function() {
+
+    const navigate = useNavigate();
+
+    function handleMineAnnoncer(event) {
+        event.preventDefault();
+        navigate("/mine-annoncer");
+    }
 
     return (
         <div className="post-created">
             <h1>Din annonce er nu oprettet.</h1>
             <img src={succes} alt="Succes" />
+            <form>
+            <button onSubmit={handleMineAnnoncer}>Mine annoncer</button>
+            </form>
         </div>
     )
 }
