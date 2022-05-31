69e054db949c34b05da4b3b5aa0c609ac2755779
mikkelhau <mikkel.hau@hotmail.com>
2022-06-01 01:24:06 +0200

    homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,15 +1,33 @@
 import { useEffect, useState } from "react";
 import logo from "../assets/img/skovl-logo.png"
+import { onSnapshot, orderBy, query } from "@firebase/firestore";
+import { postsRef } from "../firebase-config";
 
 
 
 export default function HomePage({ showLoader }) {
 
+    const [searchValue, setSearchValue] = useState("");
+    const [posts, setPosts] = useState([]);
+    const shuffledPosts = posts.sort(()=> Math.random() - Math.random());
+
+    useEffect(()=>{
+        const q = query(postsRef, orderBy("name"));
+        const unsubscribe = onSnapshot(q, data => {
+            const postsData = data.docs.map(doc => {
+                return { ...doc.data(), id: doc.id };
+            });
+            setRestaurants(postsData);
+        });
+        return () => unsubscribe();
+    }, []);
 
 return(
     <section className="page">
     <div className="grid-container">
-    <h1>homepage</h1>
+    {shuffledPosts.map(restaurant=>(
+            <ResultCard restaurant={restaurant} key={restaurant.id}/>        
+            ))}
     <img className="logo" src={logo} alt="skovl-logo" />
     </div>
     </section>
